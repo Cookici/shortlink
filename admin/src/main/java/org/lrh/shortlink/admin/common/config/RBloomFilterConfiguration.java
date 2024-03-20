fe2e258a9dbd05c254f34291527735c011634395
Cookici <632832232@qq.com>
2024-03-20 16:57:52 +0800

    feature：布隆过滤器引入

---
+++ admin/src/main/java/org/lrh/shortlink/admin/common/config/RBloomFilterConfiguration.java
@@ -0,0 +1,29 @@
+package org.lrh.shortlink.admin.common.config;
+
+import org.redisson.api.RBloomFilter;
+import org.redisson.api.RedissonClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @ProjectName: shortlink
+ * @Package: org.lrh.shortlink.admin.common.config
+ * @ClassName: RBloomFilterConfiguration
+ * @Author: 63283
+ * @Description: 布隆过滤器配置
+ * @Date: 2024/3/20 17:01
+ */
+
+@Configuration(value = "rBloomFilterConfigurationByAdmin")
+public class RBloomFilterConfiguration {
+
+    /**
+     * 防止用户注册查询数据库的布隆过滤器
+     */
+    @Bean
+    public RBloomFilter<String> userRegisterCachePenetrationBloomFilter(RedissonClient redissonClient) {
+        RBloomFilter<String> cachePenetrationBloomFilter = redissonClient.getBloomFilter("userRegisterCachePenetrationBloomFilter");
+        cachePenetrationBloomFilter.tryInit(100000000L, 0.001);
+        return cachePenetrationBloomFilter;
+    }
+}
