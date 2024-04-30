ff656d1dd1eff70a2820f7133eccb0854687b205
Cookici <632832232@qq.com>
2024-04-30 21:04:19 +0800

    feature：完成短链接高频ip访问结构

---
+++ project-core/src/main/java/org/lrh/shortlink/projectcore/dao/entity/LinkAccessLogsDO.java
@@ -0,0 +1,75 @@
+package org.lrh.shortlink.projectcore.dao.entity;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.lrh.shortlink.projectcore.common.database.BaseDO;
+
+/**
+ * @ProjectName: shortlink
+ * @Package: org.lrh.shortlink.projectcore.dao.entity
+ * @ClassName: LinkAccessLogsDO
+ * @Author: 63283
+ * @Description: 访问日志监控实体
+ * @Date: 2024/4/30 下午8:48
+ */
+
+@Data
+@TableName("t_link_access_logs")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LinkAccessLogsDO extends BaseDO {
+
+    /**
+     * id
+     */
+    private Long id;
+
+    /**
+     * 完整短链接
+     */
+    private String fullShortUrl;
+
+    /**
+     * gid
+     */
+    private String gid;
+
+    /**
+     * 用户信息
+     */
+    private String user;
+
+    /**
+     * 浏览器
+     */
+    private String browser;
+
+    /**
+     * 操作系统
+     */
+    private String os;
+
+    /**
+     * ip
+     */
+    private String ip;
+
+    /**
+     * 访问网络
+     */
+    private String network;
+
+    /**
+     * 访问设备
+     */
+    private String device;
+
+    /**
+     * 地区
+     */
+    private String locale;
+}
---
+++ project-core/src/main/java/org/lrh/shortlink/projectcore/dao/mapper/LinkAccessLogsMapper.java
@@ -0,0 +1,17 @@
+package org.lrh.shortlink.projectcore.dao.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.lrh.shortlink.projectcore.dao.entity.LinkAccessLogsDO;
+
+/**
+ * @ProjectName: shortlink
+ * @Package: org.lrh.shortlink.projectcore.dao.mapper
+ * @ClassName: LinkAccessLogsMapper
+ * @Author: 63283
+ * @Description: 访问日志监控持久层
+ * @Date: 2024/4/30 下午8:49
+ */
+
+public interface LinkAccessLogsMapper extends BaseMapper<LinkAccessLogsDO> {
+
+}
